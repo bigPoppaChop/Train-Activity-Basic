9b5e99ecd5ff114407cadc0b41474ae87e82ed05
Eric Thiel <ethiel172@gmail.com>
2019-04-09 16:22:57 -0400

    added Jumbotron

---
+++ trainLogic.js
@@ -0,0 +1,13 @@
+
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAlebel_msJC7vEXyCHtbakAfcVNl10fzE",
+  authDomain: "hw7-train-activity-basic.firebaseapp.com",
+  databaseURL: "https://hw7-train-activity-basic.firebaseio.com",
+  projectId: "hw7-train-activity-basic",
+  storageBucket: "hw7-train-activity-basic.appspot.com",
+  messagingSenderId: "1085300662209"
+};
+firebase.initializeApp(config);
